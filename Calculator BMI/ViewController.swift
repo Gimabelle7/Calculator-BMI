928b5209eac93e04a13b1faba96f0ab726271d90
Gimabelle Garcia Vasquez <gimabelle01@hotmail.com>
2023-03-28 22:54:38 -0500

    Initial Commit

---
+++ Calculator BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator BMI
+//
+//  Created by Gimabelle Garcia vasquez on 28/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
